0c16b37f3302d10e520fc7e330b686cc1480ac9a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-08 02:44:01 +0530

    ws-basic

--- websockets/src/index.ts
+++ websockets/src/index.ts
@@ -1,27 +1,20 @@
-import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+import express from 'express'
+import WebSocket, { WebSocketServer } from 'ws'
 
-const server = http.createServer(function(request: any, response: any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
+const app = express()
 
-const wss = new WebSocketServer({ server });
+app.get('/', (req, res)=>{
+    res.send('Hello world');
+})
+
+const httpServer = app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
 
   ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+    console.log('received: %s', data);
   });
 
-  ws.send('Hello! Message From Server!!');
-});
-
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
 });
