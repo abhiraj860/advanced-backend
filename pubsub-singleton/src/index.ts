4e6c8d4180af8b106a64c01cc7f521d8c597025d
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-05-03 23:08:14 +0530

    added-pubsub-manager

--- pubsub-singleton/src/index.ts
+++ pubsub-singleton/src/index.ts
@@ -1,5 +1,6 @@
 import { PubSubManger } from "./PubSubManager";
 
+// Providing data to the subscribers
 setInterval(()=>{
     PubSubManger.getInstance().addUserToStock(Math.random().toString(), "APPL");
 }, 5000);
