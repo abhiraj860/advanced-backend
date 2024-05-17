9207ac290c8d55345cf7fbf0d8518b407f4b86ab
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-05-17 23:59:56 +0530

    test

---
+++ test-in-javascript/app1/src/test/index.test.ts
@@ -0,0 +1,21 @@
+import {describe, expect, test, it} from '@jest/globals';
+import {sum} from '../index';
+
+describe('sum', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+
+
+describe('multiply', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
