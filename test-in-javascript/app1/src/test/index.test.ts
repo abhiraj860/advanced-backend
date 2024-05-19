83dedaad1d29d6ef1a33770a3c768fd37a3154c8
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-05-19 17:00:51 +0530

    nesting describes

--- test-in-javascript/app1/src/test/index.test.ts
+++ test-in-javascript/app1/src/test/index.test.ts
@@ -1,24 +1,30 @@
 import { describe, expect, it } from '@jest/globals';
 import { sum, multiply } from '../index';
 
-describe('Testing sum function', () => {
-  it('should sum 1 and 2 correctly', () => {
-    const finalAnswer = sum(1, 2);
-    expect(finalAnswer).toBe(3);
-  });
-  it('should return the sum of negative numbers correctly', ()=>{
-    const finalAnswer = sum(-1, -2);
-    expect(finalAnswer).toBe(-3);
-  });
+
+describe('Testing calculator functionality', ()=>{
+
+    describe('Testing sum function', () => {
+      it('should sum 1 and 2 correctly', () => {
+        const finalAnswer = sum(1, 2);
+        expect(finalAnswer).toBe(3);
+      });
+      it('negative correctly', ()=>{
+        const finalAnswer = sum(-1, -2);
+        expect(finalAnswer).toBe(-3);
+      });
+    });
+
+    describe('Testing multiply function', ()=>{
+      it('should multiply 4 and 5', ()=>{
+        const finalAnswer = multiply(4, 5);
+        expect(finalAnswer).toBe(20);
+      });
+      it('should multiply negative numbers correctly', ()=>{
+        const finalAnswer = multiply(8, -9);
+        expect(finalAnswer).toBe(-72);
+      });
+    });
+
 });
 
-describe('Testing multiply function', ()=>{
-  it('should multiply 4 and 5', ()=>{
-    const finalAnswer = multiply(4, 5);
-    expect(finalAnswer).toBe(20);
-  });
-  it('should multiply negative numbers correctly', ()=>{
-    const finalAnswer = multiply(8, -9);
-    expect(finalAnswer).toBe(-72);
-  });
-});
