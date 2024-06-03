9984c929732635e819db9f9b462a5ece4748bfb7
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-03 13:01:36 +0530

    solve bug

--- grpc-implementation/dist/index.js
+++ grpc-implementation/dist/index.js
@@ -29,12 +29,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const grpc = __importStar(require("@grpc/grpc-js"));
 const protoLoader = __importStar(require("@grpc/proto-loader"));
-const constants_1 = require("@grpc/grpc-js/build/src/constants");
 const packageDefinition = protoLoader.loadSync(path_1.default.join(__dirname, '../src/a.proto'));
 const personProto = grpc.loadPackageDefinition(packageDefinition);
 const PERSONS = [
     {
-        name: "harkirat",
+        name: "Abhiraj",
         age: 45
     },
     {
@@ -42,30 +41,24 @@ const PERSONS = [
         age: 45
     },
 ];
-const handler = {
-    AddPerson: (call, callback) => {
-        let person = {
-            name: call.request.name,
-            age: call.request.age
-        };
-        PERSONS.push(person);
-        callback(null, person);
-    },
-    GetPersonByName: (call, callback) => {
-        let person = PERSONS.find(x => x.name === call.request.name);
-        if (person) {
-            callback(null, person);
-        }
-        else {
-            callback({
-                code: constants_1.Status.NOT_FOUND,
-                details: "not found"
-            }, null);
-        }
-    }
-};
+// @ts-ignore
+function addPerson(call, callback) {
+    console.log(call);
+    let person = {
+        name: call.request.name,
+        age: call.request.age
+    };
+    PERSONS.push(person);
+    callback(null, person);
+}
+// @ts-ignore
+function getPersonByName(call, callback) {
+    const personName = call.request.name;
+    const p = PERSONS.find(x => x.name === personName);
+    callback(null, p);
+}
 const server = new grpc.Server();
-server.addService((personProto.AddressBookService).service, handler);
+server.addService((personProto.AddressBookService).service, { addPerson: addPerson, getPersonByName: getPersonByName });
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
     server.start();
 });
--- grpc-implementation/src/index.ts
+++ grpc-implementation/src/index.ts
@@ -3,16 +3,15 @@ import * as grpc from '@grpc/grpc-js';
 import  { GrpcObject, ServiceClientConstructor } from "@grpc/grpc-js"
 import * as protoLoader from '@grpc/proto-loader';
 import { ProtoGrpcType } from './generated/a';
-import { AddressBookServiceHandlers } from './generated/AddressBookService';
-import { Status } from '@grpc/grpc-js/build/src/constants';
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../src/a.proto'));
+
 
 const personProto = (grpc.loadPackageDefinition(packageDefinition) as unknown) as ProtoGrpcType;
 
 const PERSONS = [
     {
-        name: "harkirat",
+        name: "Abhiraj",
         age: 45
     },
     {
@@ -21,32 +20,28 @@ const PERSONS = [
     },
 ];
 
-const handler: AddressBookServiceHandlers =  {
-  AddPerson: (call, callback) => {
+// @ts-ignore
+function addPerson(call, callback) {
+  console.log(call)
     let person = {
       name: call.request.name,
       age: call.request.age
     }
     PERSONS.push(person);
     callback(null, person)
-  },
-  GetPersonByName: (call, callback) => {
-    let person = PERSONS.find(x => x.name === call.request.name);
-    if (person) {
-      callback(null, person)
-    } else {
-      callback({
-        code: Status.NOT_FOUND,
-        details: "not found"
-      }, null);
-    }
-  }
+}
+
+// @ts-ignore
+function getPersonByName(call, callback) {
+    const personName = call.request.name;
+    const p = PERSONS.find(x=> x.name === personName);
+    callback(null, p);
 }
 
 
 const server = new grpc.Server();
 
-server.addService((personProto.AddressBookService).service, handler);
+server.addService((personProto.AddressBookService).service, { addPerson: addPerson, getPersonByName: getPersonByName });
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    server.start();
+     server.start();
 });
