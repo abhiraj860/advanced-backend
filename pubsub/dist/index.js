c3bdc9dbb2d0a1e958b96ebbe5b9ac14cc612e4b
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-07-16 16:14:37 +0530

    do pubsub

--- pubsub/dist/index.js
+++ pubsub/dist/index.js
@@ -1,13 +1,65 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
-const wss = new ws_1.WebSocketServer({ port: 8080 }, () => {
-    console.log("Server running on port 8080");
+const redis_1 = require("redis");
+const publishClient = (0, redis_1.createClient)();
+publishClient.connect();
+const subscribeClient = (0, redis_1.createClient)();
+subscribeClient.connect();
+const wss = new ws_1.WebSocketServer({ port: 8081 }, () => {
+    console.log("Listening to port 8081");
 });
+const users = {};
 wss.on('connection', (userSocket) => {
-    userSocket.send("Hi you are connected to the server");
+    const id = randomId();
+    users[id] = {
+        ws: userSocket,
+        rooms: []
+    };
     userSocket.on('message', (data) => {
-        console.log("User sent a message");
-        userSocket.send("Hey you sent this messsage to the server" + data);
+        const parsedData = JSON.parse(data);
+        if (parsedData.type === "SUBSCRIBE" && !users[id].rooms.includes(parsedData.roomId)) {
+            users[id].rooms.push(parsedData.roomId);
+            // console.log(users);
+            if (oneUserSubscribedTo(parsedData.roomId)) {
+                console.log("Subscribed to redis server.");
+                subscribeClient.subscribe(parsedData.roomId, (message) => {
+                    const parsedData = JSON.parse(message);
+                    Object.keys(users).forEach((userId) => {
+                        const { ws, rooms } = users[userId];
+                        if (rooms.includes(parsedData.roomId)) {
+                            ws.send(parsedData.message);
+                        }
+                    });
+                });
+            }
+        }
+        if (parsedData.type === "sendMessage") {
+            const message = parsedData.message;
+            const roomId = parsedData.roomId;
+            console.log("Inside the send message");
+            publishClient.publish(roomId, JSON.stringify({
+                type: "sendMessage",
+                roomId: roomId,
+                message
+            }));
+        }
     });
 });
+function randomId() {
+    return Math.random();
+}
+function oneUserSubscribedTo(roomId) {
+    let totalInterestedPeople = 0;
+    // console.log(roomId);
+    Object.keys(users).map(userId => {
+        if (users[userId].rooms.includes(roomId)) {
+            // console.log("Inside the iteration");
+            totalInterestedPeople++;
+        }
+    });
+    console.log(totalInterestedPeople);
+    return true ? totalInterestedPeople === 1 : false;
+}
+function lastPersonLeftRoom(roomId) {
+}
--- pubsub/src/index.ts
+++ pubsub/src/index.ts
@@ -1,13 +1,76 @@
 import {WebSocketServer, WebSocket} from "ws";
+import {createClient} from "redis";
 
-const wss = new WebSocketServer({port:8080}, ()=>{
-    console.log("Server running on port 8080");
+const publishClient = createClient();
+publishClient.connect();
+
+const subscribeClient = createClient();
+subscribeClient.connect();
+
+const wss = new WebSocketServer({port:8081}, ()=>{
+    console.log("Listening to port 8081");
 });
+
+const users: {[key:string]: {
+    ws: WebSocket,
+    rooms: String[]
+}} = {};
+
 
 wss.on('connection', (userSocket)=>{
-    userSocket.send("Hi you are connected to the server");
-    userSocket.on('message', (data: any)=>{
-       console.log("User sent a message");
-       userSocket.send("Hey you sent this messsage to the server" + data); 
+    const id = randomId();
+    users[id] = {
+        ws: userSocket,
+        rooms: []
+    }
+    userSocket.on('message', (data)=>{
+        const parsedData = JSON.parse(data as unknown as string);
+        if(parsedData.type === "SUBSCRIBE" && !users[id].rooms.includes(parsedData.roomId)) {
+            users[id].rooms.push(parsedData.roomId);
+            // console.log(users);
+            if(oneUserSubscribedTo(parsedData.roomId)) {
+                console.log("Subscribed to redis server.");
+                subscribeClient.subscribe(parsedData.roomId, (message)=>{
+                    const parsedData = JSON.parse(message);
+                    Object.keys(users).forEach((userId)=>{
+                        const {ws, rooms} = users[userId];
+                        if(rooms.includes(parsedData.roomId)) {
+                            ws.send(parsedData.message);
+                        }
+                    });    
+                });
+            }
+        }
+        if(parsedData.type === "sendMessage") {
+            const message = parsedData.message;
+            const roomId = parsedData.roomId;
+            console.log("Inside the send message");
+            publishClient.publish(roomId, JSON.stringify({
+                type: "sendMessage",
+                roomId: roomId,
+                message
+            }))
+        }
     });
 });
+
+function randomId() {
+    return Math.random();
+}
+
+function oneUserSubscribedTo(roomId: string) {
+    let totalInterestedPeople = 0;
+    // console.log(roomId);
+    Object.keys(users).map(userId=>{
+        if(users[userId].rooms.includes(roomId)) {
+            // console.log("Inside the iteration");
+            totalInterestedPeople++;
+        }
+    });
+    console.log(totalInterestedPeople);
+    return true ? totalInterestedPeople === 1: false;
+}
+
+function lastPersonLeftRoom(roomId: string) {
+     
+}
